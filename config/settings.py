6d14b2351152182026c1f689b677aa29b74a1d8d
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2023-10-20 22:46:48 +0500

    added procfile

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^usg(v18nph&nh#at4*ssm__cj!-e#45qrql+ts_k^=)pjw8#n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
